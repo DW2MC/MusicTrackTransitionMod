81ad1e128da447018eece9eb3b10f81f0d360f2a
Tyler Young <tyler.young@impromptu.ninja>
2022-03-15 03:01:47 -0400

    initial commit

---
+++ MusicTrackTransitionMod/Mod.cs
@@ -0,0 +1,14 @@
+﻿using DistantWorlds2;
+using JetBrains.Annotations;
+
+namespace MusicTrackTransitionMod;
+
+[PublicAPI]
+public class Mod
+{
+    public Mod(DWGame game)
+    {
+        game.MusicSystem.TrackTransitionTime = 180;
+        game.MusicSystem.TrackTransitionTimeOverride = 15;
+    }
+}
